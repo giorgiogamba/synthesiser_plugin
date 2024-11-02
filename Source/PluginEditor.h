1ce81647171176cccb6338393e04908b8e242192
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-11-02 15:32:48 +0100

    juce project creation

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class Synthesiser_pluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    Synthesiser_pluginAudioProcessorEditor (Synthesiser_pluginAudioProcessor&);
+    ~Synthesiser_pluginAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Synthesiser_pluginAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Synthesiser_pluginAudioProcessorEditor)
+};

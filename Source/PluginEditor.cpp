2b39e9a284dbaba479f2b71532d5285b3a3368b1
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-11-04 22:35:30 +0100

    delete boiler plate code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,8 +13,6 @@
 Synthesiser_pluginAudioProcessorEditor::Synthesiser_pluginAudioProcessorEditor (Synthesiser_pluginAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize (400, 300);
 }
 

dac7b2f178b0383bae28d3e2f8a5a94b69f814a1
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-11-09 16:18:30 +0100

    remove juce boilerplate code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,15 +1,8 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
+// Copyright Giorgio Gamba, 2024
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
 Synthesiser_pluginAudioProcessorEditor::Synthesiser_pluginAudioProcessorEditor (Synthesiser_pluginAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
@@ -27,15 +20,9 @@ Synthesiser_pluginAudioProcessorEditor::~Synthesiser_pluginAudioProcessorEditor(
 {
 }
 
-//==============================================================================
 void Synthesiser_pluginAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void Synthesiser_pluginAudioProcessorEditor::resized()
